8120cf038baac2b522c61e08d73724a2ddcad76a
Pedro Nogueira <ngrpedro22@gmail.com>
2022-11-06 10:22:31 -0300

    footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,80 +1,132 @@
-import { ArrowRightIcon } from "@chakra-ui/icons";
-import { IconButton } from '@chakra-ui/react';
-import { FacebookLogo, InstagramLogo, TwitterLogo } from 'phosphor-react';
+import { FacebookLogo, InstagramLogo, WhatsappLogo } from "phosphor-react";
 import React from "react";
 
 const Footer = () => {
   return (
-    <div className="padding-container py-20 bg-[#56128C] grid grid-cols-1 md:grid-cols-2 gap-10">
-      <div className=" flex items-start justify-start gap-5">
-        <div>
-          <h1 className="text-xl my-2 font-bold">Quick Links</h1>
-          <ul>
-            <li className="flex items-center gap-2">
-              <ArrowRightIcon size={8} />
-              Link
-            </li>
-            <li className="flex items-center gap-2">
-              <ArrowRightIcon size={8} />
-              Link
-            </li>
-            <li className="flex items-center gap-2">
-              <ArrowRightIcon size={8} />
-              Link
-            </li>
-          </ul>
+    <footer className="relative bg-blueGray-200 pt-8 pb-6">
+        
+      <div className="container mx-auto px-4">
+        <div className="flex flex-wrap text-left lg:text-left">
+          <div className="w-full lg:w-6/12 px-4">
+            <h4 className="text-3xl fonat-semibold text-blueGray-700">
+              Let's keep in touch!
+            </h4>
+            <h5 className="text-lg mt-0 mb-2 text-blueGray-600">
+              Find us on any of these platforms, we respond 1-2 business days.
+            </h5>
+            <div className="mt-6 lg:mb-0 mb-6 space-x-5">
+              <button className="" type="button">
+                <InstagramLogo size={28} />
+              </button>
+              <button className="" type="button">
+                <FacebookLogo size={28} />
+              </button>
+              <button className="" type="button">
+                <WhatsappLogo size={28} />
+              </button>
+            </div>
+          </div>
+          <div className="w-full lg:w-6/12 px-4">
+            <div className="flex items-top mb-6">
+              <div className="w-full px-4 ml-auto">
+                <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">
+                  Useful Links
+                </span>
+                <ul className="list-unstyled">
+                  <li>
+                    <a
+                      className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                      href="#"
+                    >
+                      About Us
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                      href="#"
+                    >
+                      Blog
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                      href="#"
+                    >
+                      Github
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                      href="#"
+                    >
+                      Free Products
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div className="w-full lg:w-4/12 px-4">
+                <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">
+                  Other Resources
+                </span>
+                <ul className="list-unstyled">
+                  <li>
+                    <a
+                      className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                      href="#"
+                    >
+                      MIT License
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                      href="https://creative-tim.com/terms?ref=njs-profile"
+                    >
+                      Terms &amp; Conditions
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                      href="https://creative-tim.com/privacy?ref=njs-profile"
+                    >
+                      Privacy Policy
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                      href="https://creative-tim.com/contact-us?ref=njs-profile"
+                    >
+                      Contact Us
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
-        <div>
-          <h1 className="text-xl my-2 font-bold">Contact Indo</h1>
-          <ul>
-            <li className="flex items-center gap-2">
-              <ArrowRightIcon size={8} />
-              Menu
-            </li>
-            <li className="flex items-center gap-2">
-              <ArrowRightIcon size={8} />
-              Menu
-            </li>
-            <li className="flex items-center gap-2">
-              <ArrowRightIcon size={8} />
-              Menu
-            </li>
-          </ul>
+        <hr className="my-6 border-blueGray-300" />
+        <div className="flex flex-wrap items-center md:justify-between justify-center">
+          <div className="w-full md:w-4/12 px-4 mx-auto text-center">
+            <div className="text-sm text-blueGray-500 font-semibold py-1">
+              <a
+                href="https://www.linkedin.com/in/pedro-nogueira399/"
+                className="text-white hover:text-gray-400"
+                target="_blank"
+              >
+                {" "}
+                Pedro Soares - All Rights Reserved © | 2022
+              </a>
+              
+            </div>
+          </div>
         </div>
       </div>
-      <div className=" flex items-start justify-start gap-5">
-        <IconButton
-          aria-label="Search database"
-          rounded={"full"}
-          fontWeight={"bold"}
-          fontSize="16px"
-          lineHeight={"19pxp"}
-          bg={"#AB72D8"}
-          _hover={{ opacity: 0.8 }}
-          icon={<FacebookLogo size={28} />}
-        />
-        <IconButton
-          aria-label="Search database"
-          rounded={"full"}
-          fontWeight={"bold"}
-          fontSize="16px"
-          lineHeight={"19pxp"}
-          bg={"#AB72D8"}
-          _hover={{ opacity: 0.8 }}
-          icon={<InstagramLogo size={28} />}
-        />
-        <IconButton
-          aria-label="Search database"
-          rounded={"full"}
-          fontWeight={"bold"}
-          fontSize="16px"
-          lineHeight={"19pxp"}
-          bg={"#AB72D8"}
-          _hover={{ opacity: 0.8 }}
-          icon={<TwitterLogo size={28} />}
-        />
-      </div>
-    </div>
+    </footer>
   );
 };
 
