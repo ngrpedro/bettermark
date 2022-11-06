52ec4b8a5035ad5995ead4e68357e0fef3f8fa66
Pedro Nogueira <ngrpedro22@gmail.com>
2022-11-06 15:03:37 -0300

    banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Banners from "./sections/Banners";
+import { Button } from "@chakra-ui/react";
 import Cards from "./sections/Cards";
 import Footer from "./sections/Footer";
 import Header from "./sections/Header";
@@ -14,7 +14,45 @@ function App() {
       <NavBar />
       <Header />
       <Cards />
-      <Banners />
+      <div
+        className="padding-container relative bg-white 
+          grid grid-cols-1 md:grid-cols-2 items-center justify-center"
+      >
+        <div className="space-y-5">
+          <h1 className="block text-xl md:text-3xl font-quatt text-black">
+            Increasing your sales is always a challenge.
+          </h1>
+          <p className="text-base text-black">
+            And although everyone brags about how good online marketing is, its
+            just very hard to take the best of this opportunity if you are not
+            an expert.
+          </p>
+          <Button
+            bg={"#FF6E4F"}
+            _hover={{ opacity: 0.8 }}
+            _active={{ opacity: 0.8 }}
+            rounded={"full"}
+            py="12px"
+            px="40px"
+            fontWeight={"bold"}
+            fontSize="16px"
+            lineHeight={"19pxp"}
+          >
+            Schedule a call
+          </Button>
+          <div className="icon1 absolute top-0"></div>
+          <div className="icon2 absolute top-20 left-10"></div>
+          <div className="icon3 absolute top-28 left-96"></div>
+          <div className="icon4 absolute bottom-3 right-[45rem]"></div>
+        </div>
+
+        <div className="flex items-center justify-center text-black">
+          <div className="">
+            <div className="moca-bg">
+            </div>
+          </div>
+        </div>
+      </div>
       <Images />
       <Texts />
       <Steps />
