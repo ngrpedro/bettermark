311ff59225acb9a18cac8dadf3e84cf6dae19f2c
Pedro Soares <ngrpedro22@gmail.com>
2022-11-09 16:48:32 -0300

    services one

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -12,13 +12,95 @@ const Services = () => {
             <div className='mx-6 md:mx-24 bg-white p-8 rounded-xl -mt-16'>
                 <Tabs>
                     <TabList>
-                        <Tab>Local SEO and national SEO</Tab>
-                        <Tab>Paid advertising management</Tab>
+                        <Tab _selected={{
+                            color: "#011238",
+                            fontWeight: "700",
+                            border: "#011238",
+                            borderBottom: "2px"
+                        }}>Local SEO and national SEO</Tab>
+                        <Tab _selected={{
+                            color: "#011238",
+                            fontWeight: "700",
+                            border: "#011238",
+                            borderBottom: "2px"
+                        }}>Paid advertising management</Tab>
+
                     </TabList>
 
                     <TabPanels>
                         <TabPanel>
-                            <p>Local SEO and national SEO</p>
+                            <div className='grid grid-cols-1 md:grid-cols-2 gap-10 pt-9'>
+                                <div className='flex flex-col items-start justify-start gap-3 border-r border-gray-300'>
+                                    <div className='p-3 rounded-md bg-purple-300'>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418" />
+                                        </svg>
+                                    </div>
+
+                                    <p className='text-lg font-bold'>Site Assessment and Intake</p>
+                                    <p className='text-md max-w-sm'>
+                                        We audit your website, online presence and gather
+                                        information about your target audience and goals
+                                    </p>
+                                </div>
+
+
+                                <div className='flex flex-col items-start justify-start gap-3'>
+                                    <div className='p-3 rounded-md bg-purple-300'>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418" />
+                                        </svg>
+                                    </div>
+
+                                    <p className='text-lg font-bold'>Site Assessment and Intake</p>
+                                    <p className='text-md max-w-sm'>
+                                        We audit your website, online presence and gather
+                                        information about your target audience and goals
+                                    </p>
+                                </div>
+
+                                <div className='flex flex-col items-start justify-start gap-3 border-r border-gray-300'>
+                                    <div className='p-3 rounded-md bg-purple-300'>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418" />
+                                        </svg>
+                                    </div>
+
+                                    <p className='text-lg font-bold'>Site Assessment and Intake</p>
+                                    <p className='text-md max-w-sm'>
+                                        We audit your website, online presence and gather
+                                        information about your target audience and goals
+                                    </p>
+                                </div>
+
+                                <div className='flex flex-col items-start justify-start gap-3'>
+                                    <div className='p-3 rounded-md bg-purple-300'>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418" />
+                                        </svg>
+                                    </div>
+
+                                    <p className='text-lg font-bold'>Site Assessment and Intake</p>
+                                    <p className='text-md max-w-sm'>
+                                        We audit your website, online presence and gather
+                                        information about your target audience and goals
+                                    </p>
+                                </div>
+
+                                <div className='flex flex-col items-start justify-start gap-3'>
+                                    <div className='p-3 rounded-md bg-purple-300'>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418" />
+                                        </svg>
+                                    </div>
+
+                                    <p className='text-lg font-bold'>Site Assessment and Intake</p>
+                                    <p className='text-md max-w-sm'>
+                                        We audit your website, online presence and gather
+                                        information about your target audience and goals
+                                    </p>
+                                </div>
+                            </div>
                         </TabPanel>
                         <TabPanel>
                             <p>Paid advertising management</p>
