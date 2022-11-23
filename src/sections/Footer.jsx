83a350ab6388e2c37b67e0a724fb27147ad2fc29
Pedro Soares <ngrpedro22@gmail.com>
2022-11-23 19:39:04 -0300

    footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,13 +1,16 @@
 import { FacebookLogo, InstagramLogo, WhatsappLogo } from "phosphor-react";
 import React from "react";
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <footer className="padding-container relative bg-blueGray-200 pt-8 pb-6 bg-white 
-          text-black shadow-2xl shadow-white border-t border-gray-200">
+    <footer
+      className="padding-container relative bg-blueGray-200 pt-8 pb-6 bg-white 
+          text-black shadow-2xl shadow-white border-t border-gray-200"
+    >
       <div className="mx-auto">
-        <div className="flex flex-wrap text-left lg:text-left">
-          <div className="w-full lg:w-6/12">
+        <div className="flex items-center justify-between w-full">
+          <div className="w-full ">
             <h4 className="text-3xl fonat-semibold text-blueGray-700">
               Let's keep in touch!
             </h4>
@@ -26,84 +29,25 @@ const Footer = () => {
               </button>
             </div>
           </div>
-          <div className="w-full lg:w-6/12">
+          <div className="w-full">
             <div className="flex items-top mb-6">
               <div className="w-full ml-auto">
                 <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">
                   Useful Links
                 </span>
-                <ul className="list-unstyled">
-                  <li>
-                    <a
-                      className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                      href="#"
-                    >
-                      About Us
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                      href="#"
-                    >
-                      Blog
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                      href="#"
-                    >
-                      Github
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                      href="#"
-                    >
-                      Free Products
-                    </a>
-                  </li>
-                </ul>
-              </div>
-              <div className="w-full lg:w-4/12 px-4">
-                <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">
-                  Other Resources
-                </span>
-                <ul className="list-unstyled">
-                  <li>
-                    <a
-                      className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                      href="#"
-                    >
-                      MIT License
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                      href="https://creative-tim.com/terms?ref=njs-profile"
-                    >
-                      Terms &amp; Conditions
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                      href="https://creative-tim.com/privacy?ref=njs-profile"
-                    >
-                      Privacy Policy
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                      href="https://creative-tim.com/contact-us?ref=njs-profile"
-                    >
-                      Contact Us
-                    </a>
-                  </li>
+                <ul className="list-unstyled flex gap-5">
+                  <Link to="/">
+                    <li>Home</li>
+                  </Link>
+                  <Link to="/Services">
+                    <li>Services</li>
+                  </Link>
+                  <Link to="/Results">
+                    <li>Results</li>
+                  </Link>
+                  <Link to="/ContactUs">
+                    <li>Contact Us</li>
+                  </Link>
                 </ul>
               </div>
             </div>
